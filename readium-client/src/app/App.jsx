9138a51bf3ae85eec426e75fbc45b86d0ca58ff8
Hoàng Vũ <59165903+hoangvu03081@users.noreply.github.com>
2021-11-15 20:20:20 +0700

    Update App.jsx

--- readium-client/src/app/App.jsx
+++ readium-client/src/app/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Body from "../common/components/Body";
 import Header from "../common/components/Header";
 import MobileNavbar from "../common/components/Header/MobileNavbar";
 import SignInModal from "../common/components/SignInModal";
