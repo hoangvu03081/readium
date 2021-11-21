7eaf2d13e2f11602765a1818fa863af4f9337f87
tltson299 <truongson1128@gmail.com>
2021-11-21 22:58:53 +0700

    frontend - Update Responsive UI

--- readium-client/src/common/components/Body/index.jsx
+++ readium-client/src/common/components/Body/index.jsx
@@ -8,24 +8,21 @@ const Layout = styled.div`
   flex-direction: row;
   margin-top: 80px;
   justify-content: space-between;
-  @media (max-width: 1399px) {
-    padding: 0 30px 0 80px;
-  }
-  @media (max-width: 1299px) {
-    padding: 0 30px 0 60px;
-  }
 `;
 
 const ColumnLeft = styled.div`
-  padding: ${(props) => (props.isMobile ? "48px 36px" : "30px 80px 0 0")};
+  padding: ${(props) => (props.isMobile ? "48px 36px" : "30px 80px 13px")};
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   border-right: 2px solid black;
   min-height: 100vh;
+  @media (max-width: 1399px) {
+    padding: 30px 80px 30px 68px;
+  }
   @media (max-width: 1299px) {
-    padding: 30px 60px 0 0;
+    padding: 30px 60px 30px 48px;
   }
   @media (max-width: 1199px) {
     margin-right: 0;
@@ -37,27 +34,36 @@ const ColumnLeft = styled.div`
   }
 `;
 
-const ColumnRight = styled.div`
-  width: 380px;
-  height: calc(100vh - 80px);
-  overflow-y: scroll;
-  padding: 30px 18px 30px 48px;
-`;
-
-const FixedRight = styled.div`
+const FixedScroll = styled.div`
   position: -webkit-sticky;
   position: sticky;
   top: 80px;
   height: 0;
 `;
 
+const ColumnRight = styled.div`
+  width: 380px;
+  height: calc(100vh - 80px);
+  padding: 30px 30px 30px 42px;
+  overflow-y: scroll;
+  overflow: -moz-scrollbars-none;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  @media (min-width: 1400px) {
+    width: 420px;
+    padding: 30px 48px;
+  }
+`;
+
 export default function Body({ contentLeft, contentRight }) {
   return (
     <Layout className={isMobile ? "" : "container-xxl"}>
       <ColumnLeft isMobile={isMobile}>{contentLeft}</ColumnLeft>
-      <FixedRight className="d-none d-xl-block">
+      <FixedScroll className="d-none d-xl-block">
         <ColumnRight>{contentRight}</ColumnRight>
-      </FixedRight>
+      </FixedScroll>
     </Layout>
   );
 }
