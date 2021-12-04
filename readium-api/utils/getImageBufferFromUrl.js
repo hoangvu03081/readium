7282f19ac73cc97891a4f0163766a61a4536b3ed
TruongQuyHai <87081336+TruongQuyHai@users.noreply.github.com>
2021-12-04 11:28:07 +0700

    Create a default user's avatar

--- readium-api/utils/getImageBufferFromUrl.js
+++ readium-api/utils/getImageBufferFromUrl.js
@@ -1,36 +1,34 @@
 const axios = require("axios").default;
-const EventEmitter = require("events").EventEmitter;
+const sharp = require("sharp");
 
-// create an event emitter
+const { once, EventEmitter } = require("events");
 const bufferEmitter = new EventEmitter();
 
-// * Ex:
-//  + on downloaded event event,
-//  + we receive a buffer of the image
-//    bufferEmitter.on("downloaded", (buf) => {
-//      console.log(buf);
-//    });
-
-// This is a stream so use then, catch
 function getImageBufferFromUrl(url) {
   axios.get(url, { responseType: "stream" }).then((res) => {
-    // create an array of buffer
     const bufs = [];
 
     res.data.on("data", (data) => {
       bufs.push(data);
     });
 
-    // join the array of buffer into one buffer object
     res.data.on("end", () => {
       const buf = Buffer.concat(bufs);
-      // emit 'downloaded' event to return buf
       bufferEmitter.emit("downloaded", buf);
     });
   });
 }
 
+const downloadImageFromUrl = (url) => {
+  getImageBufferFromUrl(url);
+  return once(bufferEmitter, "downloaded");
+};
+
+const convertBufferToPng = (buffer) => {
+  return sharp(buffer).png().toBuffer();
+};
+
 module.exports = {
-  bufferEmitter,
-  getImageBufferFromUrl,
+  downloadImageFromUrl,
+  convertBufferToPng,
 };
