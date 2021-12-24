1934067b0f5135345d08c16670dd5da9231d2bb1
nnkhang19 <nnkhang735@gmail.com>
2021-12-24 13:27:07 +0700

    fix bug ai

--- readium-ai-service/app.py
+++ readium-ai-service/app.py
@@ -21,7 +21,19 @@ channel.queue_declare(queue=QUEUE, durable=True)
 
 
 for method_frame, properties, body in channel.consume(QUEUE):
+
+
+    # check existence of id in DB
+
+
     print(method_frame, properties, body)
+    body = dict(body[2:len(body) - 1])
+    id_= body['id']
     with open("myfile.txt", "w") as file1:
-        file1.write(str(body))
+        file1.write(str("haha"))
+
+
+    # check existence if yes -> save, else stop continue
+
+    # important acknowledgement
     channel.basic_ack(method_frame.delivery_tag)
