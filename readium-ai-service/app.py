93de8441de4fd62a8332e5318ab4002a244d3540
nnkhang19 <nnkhang735@gmail.com>
2021-12-27 15:26:14 +0700

    add models

--- readium-ai-service/app.py
+++ readium-ai-service/app.py
@@ -11,6 +11,10 @@
 #     app.run()
 
 import pika
+import json
+import os
+from mongo_model import *
+from utils import *
 
 QUEUE = "ai_task_queue"
 parameters = (pika.ConnectionParameters(host='readium-rabbit', port='5672', connection_attempts=50, retry_delay=2))
@@ -20,8 +24,42 @@ channel = connection.channel()
 channel.queue_declare(queue=QUEUE, durable=True)
 
 
+mongo = ReadiumDB()
+
+model = create_model()
+
+print("before loop")
+
+
 for method_frame, properties, body in channel.consume(QUEUE):
-    print(method_frame, properties, body)
+
+
+    # check existence of id in DB
+
+    body = body.decode('utf-8')
+    body = json.loads(body)
+    id_ = str(body['id'])
+
+    try:
+        a = mongo.get_post_by_id(id_)
+        content = a['content']
+        chunked_text = create_chunk(content)
+        summary = summarize(model, content)
+    except :
+        a = 2
+        summary = "haha"
+
     with open("myfile.txt", "w") as file1:
-        file1.write(str(body))
+        file1.write("hello\n")
+        file1.write(id_)
+        file1.write('\n')
+        file1.write(summary)
+        #file1.write('\n')
+        #file1.write(str(mongo.db) + '\n')
+        #file1.write(str(mongo.posts))
+
+
+    # check existence if yes -> save, else stop continue
+
+    # important acknowledgement
     channel.basic_ack(method_frame.delivery_tag)
