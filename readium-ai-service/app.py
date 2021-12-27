88398190dd430666e8b9bdbb6de12b3ffa8902ab
nnkhang19 <nnkhang735@gmail.com>
2021-12-27 15:00:38 +0700

    add model

--- readium-ai-service/app.py
+++ readium-ai-service/app.py
@@ -14,6 +14,7 @@ import pika
 import json
 import os
 from mongo_model import *
+from utils import *
 
 QUEUE = "ai_task_queue"
 parameters = (pika.ConnectionParameters(host='readium-rabbit', port='5672', connection_attempts=50, retry_delay=2))
@@ -25,6 +26,8 @@ channel.queue_declare(queue=QUEUE, durable=True)
 
 mongo = ReadiumDB()
 
+model = create_model()
+
 print("before loop")
 
 
@@ -33,14 +36,27 @@ for method_frame, properties, body in channel.consume(QUEUE):
 
     # check existence of id in DB
 
-
-    print(body)
     body = body.decode('utf-8')
     body = json.loads(body)
     id_ = str(body['id'])
-    print(id_)
+
+    try:
+        a = mongo.get_post_by_id(id_)
+        content = a['content']
+        chunked_text = create_chunk(content)
+        summary = summarize(model, content)
+    except :
+        a = 2
+        summary = "haha"
+
     with open("myfile.txt", "w") as file1:
+        file1.write("hello\n")
         file1.write(id_)
+        file1.write('\n')
+        file1.write(summary)
+        #file1.write('\n')
+        #file1.write(str(mongo.db) + '\n')
+        #file1.write(str(mongo.posts))
 
 
     # check existence if yes -> save, else stop continue
--- readium-ai-service/mongo_model.py
+++ readium-ai-service/mongo_model.py
@@ -1,5 +1,6 @@
 import os 
 from pymongo import MongoClient
+from bson import ObjectId
 
 class ReadiumDB:
 	def __init__(self):
@@ -10,20 +11,16 @@ class ReadiumDB:
 		port     = os.getenv("MONGO_PORT")
 
 		connectLink = f"mongodb://{username}:{password}@{hostname}:{port}/{dbname}?authSource=admin"
-		print(connectLink)
 		self.moConn = MongoClient(connectLink)
-		print(self.moConn)
-		#self.moConn = MongoClient("mongodb://<username>:<password>@<hostname>:<port>/<mongo_database_name>?authSource=admin")
-		#self.db = self.moConn.readium
-		#self.collection = self.db.readium
-		#print(self.collection)
-		print("haha")
+		self.db = self.moConn[dbname]
+		self.posts = self.db.posts
+		#print(self.posts)
 
 	def insert(self, id_, data):
 		pass
 
 	def get_post_by_id(self, id_):
-		return  self.collection.find_one({'id_' : id_}) 
+		return  self.posts.find_one({'_id' : ObjectId("61c961f12f67914336c86592")}) 
 
 #db = ReadiumDB()
 #print(db.get_post_by_id('123'))
